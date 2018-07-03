3128618a6b283d81f3384c7818dbb756750eb170
Oleh Bai <oleh.bai@eliftech.com>
2018-07-03 12:34:35 +0300

    added plate identifying from terminal

--- src/openALPR.js
+++ src/openALPR.js
@@ -1,4 +1,6 @@
-const openalpr = require("node-openalpr");
+const openalpr = require("node-openalpr"),
+  fs = require('fs'),
+  exec = require('child_process').exec;
 
 const path = `${__dirname}/tests/ua1.jpg`;
 
@@ -16,7 +18,6 @@ module.exports.identifyMocked = (pathToImage = path) => new Promise((resolve, re
       console.log('\nLicense recognizing is finished !!!\n');
       if (error) return reject(error)
       return resolve(output)
-      // console.log("output", output);
     })
 );
 
@@ -29,3 +30,12 @@ module.exports.identifyFromStreamStart = stream => {
 module.exports.identifyFromStreamStop = () => {
   openalpr.StopQueue()
 }
+
+module.exports.identifyPlateFromImage = ({ pathToImage, region = 'eu', country = 'ua' }) => new Promise((resolve, reject) => {
+  if (pathToImage && !fs.existsSync(pathToImage)) return reject(`File doesn't exist`);
+  const cmd = `alpr -c ${region} -p ${country} ${pathToImage || path} -j`;
+  exec(cmd, (err, result) => {
+    if (err) return reject(err);
+    resolve(JSON.parse(result));
+  })
+})
